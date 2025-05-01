1181a4781ed7602805e8f4f057ac2d622457a248
Ayomide Oluwatola <ayomideoluwatola1@gmail.com>
2025-05-01 21:05:04 +0100

    second edit readme

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
                 <img
                   src={`${connector.icon ? connector.icon : 'icons/'+connector.name+'.svg'}`}
                   alt={connector.name}
-                  className="w-6 h-6 bg-orange-500 rounded-full"
+                  className="w-6 h-6 bg-orange-500 rounded-full hover:rounded-none"
                 />
               </button>
             ))}
